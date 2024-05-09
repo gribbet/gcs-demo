5e9f34a67727a6dd54d2b6791265f4a3e90c677f
Graham <graham.gibbons@gmail.com>
2024-05-09 13:27:15 -0700

    Telemetry

--- ui/src/aircraft.ts
+++ ui/src/aircraft.ts
@@ -1,16 +1,27 @@
+import { common } from "node-mavlink";
+
 import type { Mavlink } from "./mavlink";
 import type { Aircraft, Position, State } from "./model";
 
 export const createAircraft = (mavlink: Mavlink) => {
-  const destroy = mavlink.read(data => {
-    console.log(data);
-  });
-
   const state: State = {
     time: Date.now(),
     position: [-121, 37, 1000],
     orientation: [0, 0, 0],
   };
+
+  const destroy = mavlink.read(message => {
+    state.time = Date.now();
+    if (message instanceof common.GlobalPositionInt) {
+      const latitude = message.lat / 1e7;
+      const longitude = message.lon / 1e7;
+      const altitude = message.alt / 1e3;
+      state.position = [longitude, latitude, altitude];
+    } else if (message instanceof common.Attitude) {
+      const { pitch, yaw, roll } = message;
+      state.orientation = [pitch, yaw, roll];
+    }
+  });
 
   return {
     get state() {
